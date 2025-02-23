9cc00cf1649eda000b835bb9328fe0209020c6db
Felipe Bertelli dos Santos <42683582+fsbertelli@users.noreply.github.com>
2025-02-23 16:08:54 -0300

    Commit Exercicio2/3

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Funcionario // Classe Funcionario
+{
+    public string Nome = ""; // Atributo Nome
+    public float SalarioBase = 0; // Atributo SalarioBase
+
+    public Funcionario(string nome, float SalarioBase) // Construtor da classe Funcionario
+    {
+        this.Nome = nome; // Atribui o valor de nome ao atributo Nome
+        this.SalarioBase = SalarioBase; // Atribui o valor de SalarioBase ao atributo SalarioBase
+    }
+}
+class Gerente : Funcionario // Classe Gerente herda de Funcionario
+{
+    public Gerente(string nome, float SalarioBase) : base(nome, SalarioBase) { } // Construtor da classe Gerente
+    public void calculaSalario(float bonus) // Método calculaSalario
+    {
+        Console.WriteLine($"O gerente {Nome} " +
+            $"recebe o salário base R$:{SalarioBase} " +
+            $"e bonus R${bonus} " +
+            $"totalizando: R${SalarioBase + bonus}"); // Exibe o salário total do gerente
+    }
+}
+class Program // Classe Principal
+{
+    static void Main() // Método Principal
+    {
+        Gerente func = new Gerente("Felipe", 1000);  // Instancia um objeto func da classe Gerente com atributos Nome e SalarioBase
+        func.calculaSalario(150); // Chama o método calculaSalario com o parâmetro bônus 150
+    }
+}
---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿abstract class Veiculo //Declaração da classe abstrata Veiculo
+{
+    public abstract string Acelerar(); //Declaração do método abstrato Acelerar
+}
+class Carro : Veiculo //Declaração da classe Carro herdando Veiculo
+{
+    public override string Acelerar() //Implementação do método Acelerar sobrescrevendo o método abstrato da classe Veiculo
+    {
+        return "O carro está acelerando";  //Retorna a mensagem personalizada para a classe Carro
+    }
+}
+class Moto : Veiculo //Declaração da classe Moto herdando Veiculo
+{ 
+    public override string Acelerar() //Implementação do método Acelerar sobrescrevendo o método abstrato da classe Veiculo
+    {
+        return "A moto está acelerando"; //Retorna a mensagem personalizada para a classe Moto
+    }
+}
+class Program //Declaração da classe principal Program
+{
+    static void Main() //Método principal Main
+    {
+        Carro carro = new Carro(); //Instância do objeto carro da classe Carro
+        Console.WriteLine(carro.Acelerar()); //Exibe a mensagem personalizada da classe Carro herdada de Veiculo
+        Moto moto = new Moto(); //Instância do objeto moto da classe Moto
+        Console.WriteLine(moto.Acelerar()); //Exibe a mensagem personalizada da classe Moto herdada de Veiculo
+    }
+}
